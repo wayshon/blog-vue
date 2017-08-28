c41448c857e6a719233968796822e9685743ec58
王旭 <wayshon@wangxudeMacBook-Pro.local>
2017-08-28 10:25:38 +0800

    cell 样式

--- src/components/ArticleCell.vue
+++ src/components/ArticleCell.vue
@@ -5,16 +5,21 @@
             <span>隔壁老王</span> - <span>2017-08-25 16:31</span>
         </header>
         <article @click="article">
-            <div>{{value.title}}</div>
-            {{value.content.slice(20)}}...
+            <div class="cell-title-content">
+              <h3 class="cell-title">{{value.title}}</h3>
+              <h5 v-for="(obj ,index) of value.tags" class="cell-label" :style="{borderColor:choose(obj.name)}">{{obj.name}}</h5>
+            </div>
+            <p class="cell-content">{{value.content}}</p>
         </article>
-        <footer>
-            <span>八卦新闻</span> <span>13次阅读</span> <span>13次收藏</span> <span>13次赞</span> <span>13评论</span>
+        <footer class="cell-footer">
+            <h5 class=""></h5>
+            <span>13次阅读</span> <span>13次收藏</span> <span>13次赞</span> <span>13评论</span>
         </footer>
     </div>
 </template>
 
 <script>
+  let colors = ["#9DDBFB", "#DE96E4", "#BEC8EB", "#93E1DE", "#CDB7AE", "#BECED5", "#FFBA8D", "#97E1E9", "#EDB5B5", "#FFDB70"];
   export default{
     props: {
       value: {
@@ -29,6 +34,48 @@
       article() {
         this.$emit('article', this.value);
       },
+      choose(val) {
+        let result = '';
+        if (typeof val === 'string') {
+          result = val.charCodeAt(0).toString()
+          let colorsign = parseInt(result.charAt(result.length - 1))
+          let color = colors[colorsign]
+          return color
+        } else {
+          return "#F5F6F6"
+        }
+      },
     }
   }
 </script>
+<style lang="less" scoped>
+  .cell-title-content {
+    display: flex;
+  }
+
+  .cell-title {
+    margin-left: 10px;
+  }
+
+  .cell-label {
+    margin-left: 10px;
+    border: 2px #fff solid;
+    border-radius: 5px;
+  }
+
+  .cell-content {
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+  }
+
+  .cell-footer {
+    display: flex;
+  }
+
+  .cell-footer-content {
+    
+  }
+
+</style>
